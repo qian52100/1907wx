afcc983a278923e35b42970c88770bd0545e8ca2
liqianqian <1142970858@qq.com>
2020-02-03 17:12:13 +0800

    提交

--- routes/web.php
+++ routes/web.php
@@ -66,8 +66,6 @@ Route::get('wx/auth','WeixinController@auth');  //接收code
 
 Route::any('wx/gitpull','WeixinController@gitpull');  //自动上线
 
-//Route::any('weixin/index','WeixinController@index');  //微信接口
-
 Route::any('/createMenu','WeixinController@createMenu');  //菜单
 Route::any('access_token','WeixinController@access_token');  //获取access_token
 Route::any('login/test','Admin\LoginController@test');  //模板消息
@@ -75,9 +73,3 @@ Route::any('login/test','Admin\LoginController@test');  //模板消息
 Route::any('/user_login','Admin\LoginController@user_login');  //用户绑定登陆
 
 Route::any('/user_index','Admin\LoginController@user_index');  //入库 openid
-
-
-//总结
-Route::any('/wx/index','WxController@index'); //微信接口
-Route::any('weix/gitpull','WxController@gitpull');  //自动上线
-Route::any('access_token','WxController@access_token');  //获取access_token
